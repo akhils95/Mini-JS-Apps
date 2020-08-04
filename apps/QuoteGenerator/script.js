f752caf136874645eb5e11290d6f1c271a04577c
Akhil Singh Chauhan <akhilischauhan@gmail.com>
2020-08-04 23:31:11 +0530

    added custom countdown

--- apps/QuoteGenerator/script.js
+++ apps/QuoteGenerator/script.js
@@ -19,7 +19,7 @@ function hideLoader() {
 
 async function getQuote() {
 	showLoader();
-	const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+	const proxyUrl = 'https://proxy-server-95.herokuapp.com/';
 	const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 	try{
 		const response = await fetch(proxyUrl + apiUrl);
